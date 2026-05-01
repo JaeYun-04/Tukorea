ee2f8e584df47d2eb1828fc938fd6e165d44781b
JaeYun <127004046+JaeYun-04@users.noreply.github.com>
2026-05-01 20:01:53 +0900

    Delete OOP/Jaeyun_INV directory

--- OOP/Jaeyun_INV/SourceFiles/Product.cpp
+++
@@ -1,9 +0,0 @@
-#include "Product.h"
-#include <iomanip>
-
-Product::Product(string n, int p, int s) : name(n), price(p), stock(s), active(true) {}
-
-void Product::printInfo() const { 
-    cout << left << setw(15) << name << " | 가격: " << setw(7) << price 
-         << " | 재고: " << setw(5) << stock << " | 상태: " << (active ? "판매중" : "품절(삭제)") << endl;
-}
